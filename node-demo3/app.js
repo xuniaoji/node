4a454b69499fdab137414d19def7762548824b88
xu_shuming <xu_shuming@hoperun.com>
2018-11-12 18:07:18 +0800

    demo3

---
+++ node-demo3/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('product')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/productAdd', (req, res) => {
+    res.render('productAdd')
+})
+
+app.get('/productEdit', (req, res) => {
+    res.render('productEdit')
+})
+
+app.listen(3000)
